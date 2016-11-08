9764ded3f05a3fbda37a0194c13afb54af9f51e0
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-11-08 14:50:37 -0500

    converted column tranformation function to general transformation function

--- funcs.py
+++ funcs.py
@@ -1,15 +1,26 @@
 import requests, re, base64
 import pandas as pd
 
-# TODO fix this
-def filter_empty_rows(data, rows):
-    for row in rows:
-        data = data[data[row].notnull()]
+# TODO fix this - .apply?
+
+def transform_columns (data, fn, columns ):
+    if isinstance(columns, str):
+        columns = [columns]
+        
+    for column in columns:
+        data[column] = data[column].map(fn)
+    return data
+
+
+def filter_empty_rows(data, columns):
+    for column in columns :
+        data = data[data[column].notnull()]
     return data
 
 def extract_digits (string):
     digit_list = re.findall(r'\d+', string)
     return ''.join( digit_list)
+
 def validate_phone_number(phone_number):
     # coerce into string, in case dataframe feeds it an int
     phone_number = str(phone_number)
@@ -21,26 +32,22 @@ def validate_phone_number(phone_number):
         return '1'+digits
     else:
         return ''
-def validate_all_phone_nums(data, phone_num_column='Cell number'):
-    new_phone_num_column = data[phone_num_column].map(validate_phone_number)
-    data[phone_num_column] = new_phone_num_column
-    return data
 
-def get_alpha_num_digits (string):
-    pass
+def extract_alpha_num_digits (string):
+    string = str(string)
+    return re.sub('[\W_]', '', string)
+
 
 def send_to_api(encoded_csv):
     pass
 
 def import_csv():
     return pd.read_excel('test_file.xlsx')
+
 def finished_message(success_csv, fail_csv, contats):
     pass
 
-def set_all_languages(data, language_column = '@@cci_language@@'):
-    new_language_column = data[language_column].map(set_language)
-    data[language_column] = new_language_column
-    return data
+
 def set_language(lang_string, default = 'english'):
     if lang_string.lower() == 'spanish':
         return 'spanish'
--- tests.py
+++ tests.py
@@ -4,30 +4,28 @@ from funcs import *
 class Tests(unittest.TestCase):
 
     def test_validate_phone_number (self):
+
+            self.assertEqual(validate_phone_number  ('1-555-123-4567'), '15551234567')
+            self.assertEqual(validate_phone_number  (' 555  123/4567  '), '15551234567')
+            self.assertEqual(validate_phone_number  ('555-123---4567'), '15551234567')
+
             self.assertFalse(validate_phone_number(''))
-            self.assertEqual(validate_phone_number(
-            '1-555-123-4567'), '15551234567')
-            self.assertEqual(validate_phone_number(
-            ' 555 123 4567  '), '15551234567')
-            self.assertEqual(validate_phone_number(
-            '555-123---4567'), '15551234567')
-            self.assertEqual(validate_phone_number(
-            '!11@2#3$4%5^&6*7(8)9:0;'),'11234567890')
+            self.assertFalse(validate_phone_number('123-4567'))
+            self.assertFalse(validate_phone_number('555-5555-55555'))
             for _ in range(10):
                 rand_digit_string = str(random.randint(1000000000,9999999999))
-                self.assertEqual(validate_phone_number(
-                rand_digit_string), '1' +rand_digit_string )
-    def test_get_alpha_num_digits(self):
-        self.assertEqual(get_alpha_num_digits(
-        'usa56.'),'usa' )
-
+                self.assertEqual(validate_phone_number(rand_digit_string), '1' +rand_digit_string )
+                
+    def test_extract_alpha_num_digits(self):
+        self.assertFalse('')
+        self.assertEqual(extract_alpha_num_digits   ('1234567890!@#$%^&*()'), '1234567890' )
+        self.assertEqual(extract_alpha_num_digits   ('{}|:"<>?"asdfghjkl'), 'asdfghjkl' )
     def test_set_language(self):
         self.assertEqual(set_language   (''),'english')
         self.assertEqual( set_language  ('spanish'), 'spanish')
         self.assertEqual(set_language   ('SPANISH'),'spanish')
         for _ in range(10):
-            self.assertEqual( set_language(
-            self.randomstring(8)),'english' )
+            self.assertEqual( set_language  (self.randomstring(8)),'english' )
 
     def randomstring(self, length):
         return ''.join(random.choice(string.ascii_letters) for i in range(length))
